c7b2facd0d2620c22a31c0cb060abe5637af3e3d
Varnesh1 <100467880+Varnesh1@users.noreply.github.com>
2024-02-03 11:56:48 +0000

    Add files via upload

---
+++ js/addButtons.js
@@ -0,0 +1,8 @@
+AFRAME.registerComponent("createbuttons", {
+  init: function() {
+    var button1 = document.createElement("button1");
+    button1.setAttribute("id", "order-button");
+    button1.setAttribute("class", "btn btn-danger ml-3 mr-3");
+
+  }
+});
